50b8d2f9d163841e24be4c6606d350ae9c2c768a
Emoji <55009343+Emojigit@users.noreply.github.com>
2023-01-24 22:21:17 +0800

    update\!\!

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# lava_n_water
-Minetest mod to make lava cool to special stone, easier to clean with worldedit!
+# Once upon a time, there was a incident...
+That was a long time ago, when somebody placed a bucket of water, and then lava, on a tall mountain near urban areas. The liquid created a swarm of lavastones wherever it flowed, and when the moderators arrived, they couldn't clear them all with their [adeptus powers](https://content.minetest.net/packages/sfan5/worldedit/)<!-- Well, even Morax, the Geo Archon, cannot help... --> and had to dig them one by one with their pickaxe.
+
+Why can't adeptus powers be used? That’s because if //replace all the stones with air, the entire tall mountain where the incident starts will be hollowed out, and that was not what the moderators wanted to see.
+
+Using this mod, all lavastones will be "tagged,"  so that WorldEdit or other world cleanup tools can identify them and only clear them. Moderators can then use //replace lnw air to clear all lavastones. By removing the liquid sources before executing this command, this should clear up any disasters.
+
+## Technical Details
+This mod registers one node, `lava_n_water:lava_water_aftermath_stone`, which is the lavastone. When flowing lava contact water, it will cool down into this form of stone instead of normal stone.
+
+Three alias, `lava_n_water`, `lavawater`, and `lnw`, are registered to point to the said node. This is for easier cleanup with WorldEdit.
--- init.lua
+++ init.lua
@@ -1,31 +1,16 @@
-function shallowcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in pairs(orig) do
-            copy[shallowcopy(orig_key)] = shallowcopy(orig_value)
-        end
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-end
-
-local amst_def = shallowcopy(minetest.registered_nodes["default:stone"])
+local amst_def = table.copy(minetest.registered_nodes["default:stone"])
 amst_def.description = "Lava + Water = Stone"
 amst_def.groups.not_in_creative_inventory = 1
-amst_def.groups.not_blocking_trains = 1
-amst_def.on_place = function(itemstack, placer, pointed_thing)
+amst_def.groups.not_blocking_trains = 1 -- as moderators' request on LinuxForks - avoid path blocking
+amst_def.on_place = function(itemstack, placer, pointed_thing) -- Replace all LNWs in inventory to normal stone
 	itemstack:set_name("default:stone")
 	minetest.item_place(itemstack, placer, pointed_thing)
 	return itemstack
 end
--- print(type(amst_def))
--- print(dump(amst_def))
 minetest.register_node("lava_n_water:lava_water_aftermath_stone",amst_def)
 
-
+-- Override lava cooling code -> override the relevant steps
+-- Codes from default
 default.cool_lava = function(pos, node)
 	if node.name == "default:lava_source" then
 		minetest.set_node(pos, {name = "default:obsidian"})
@@ -36,6 +21,7 @@ default.cool_lava = function(pos, node)
 		{pos = pos, max_hear_distance = 16, gain = 0.25}, true)
 end
 
+-- Alias for WorldEdit
 minetest.register_alias("lava_n_water", "lava_n_water:lava_water_aftermath_stone")
 minetest.register_alias("lavanwater", "lava_n_water:lava_water_aftermath_stone")
 minetest.register_alias("lnw", "lava_n_water:lava_water_aftermath_stone")
